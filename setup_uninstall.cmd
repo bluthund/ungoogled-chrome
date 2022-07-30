c8cb8bb7c77e245fff3905d535c7b38d8d78ce23
bluthund <32809089+bluthund@users.noreply.github.com>
2022-07-30 16:45:48 +0530

    Updating to 103.0.5060.114

--- setup_uninstall.cmd
+++ setup_uninstall.cmd
@@ -1,33 +1,52 @@
 @echo off
+color 0e
+title Ungoogled Chrome: Uninstaller
+taskkill /f /im "chrome.exe"
 
-taskkill /f /im "chrome.exe" >NUL 2>&1
+:installvars
+set "CHRVer=103.0.5060.114"
+set "CHRExec_REG=%LOCALAPPDATA%\Chromium\Application"
 
-rmdir /s /q "%LOCALAPPDATA%\Chromium\Application\93.0.4577.82" >NUL 2>&1
-ren "%LOCALAPPDATA%\Chromium\User Data" "User Data_OLD" >NUL 2>&1
+echo %chrexec_Reg% && pause
 
-del /f /s /q "%USERPROFILE%\Desktop\Google Chrome.lnk" >NUL 2>&1
-del /f /s /q "%APPDATA%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Google Chrome.lnk" >NUL 2>&1
+:keepdata
+cls && echo /
+set "proceed= " && set /p "proceed=> Press [ENTER] to remove all userdata files, or [K] and then [ENTER] to keep them: "
+if /i "%proceed%" EQU "K" (goto uninstall) else if "%proceed%" EQU " " (goto fullclean) else (goto keepdata)
 
-for /f "tokens=2* skip=2" %%a in ('reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /v "Favorites"') do set "TBPin=%%b" >NUL 2>&1
-reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /v "Favorites" /t REG_BINARY /d %TBPin:474F4F474C457E312E4C4E4B=% /f >NUL 2>&1
+:fullclean
+rmdir /s /q "%CHRExec_REG%\..\User Data" > NUL 2>&1
 
-reg delete "HKCU\SOFTWARE\RegisteredApplications" /v "Google Chrome" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Clients\StartMenuInternet\Chrome" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Classes\Chrome" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Classes\Chrome.HTML" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Classes\Chrome.XHTML" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Classes\Chrome.SHTML" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Classes\Chrome.PDF" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Classes\Chrome.SVG" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Classes\Chrome.WEBP" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\FTP\UserChoice" /v "ProgID" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice" /v "ProgID" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice" /v "ProgID" /f >NUL 2>&1
+:uninstall
+del /f /q "%CHRExec_REG%\chrome.exe" > NUL 2>&1
+del /f /q "%USERPROFILE%\Desktop\Google Chrome.lnk" > NUL 2>&1
+del /f /s /q "%APPDATA%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Google Chrome.lnk" > NUL 2>&1
 
-reg delete "HKCU\SOFTWARE\Chrome" /f >NUL 2>&1
-reg delete "HKCU\SOFTWARE\Chromium" /f >NUL 2>&1
+rmdir /s /q "%CHRExec_REG%\%CHRVer%" > NUL 2>&1
+ren "%CHRExec_REG%\..\User Data_OLD" "User Data" > NUL 2>&1
+rmdir /q "%CHRExec_REG%" > NUL 2>&1
+rmdir /q "%CHRExec_REG%\.." > NUL 2>&1
 
-taskkill /f /im "explorer.exe" >NUL 2>&1
-start "" "explorer.exe" >NUL 2>&1
-explorer "%~dp0" >NUL 2>&1
+for /f "tokens=2* skip=2" %%a in ('reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /v "Favorites"') do set "TBPin=%%b" > NUL 2>&1
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /v "Favorites" /t REG_BINARY /d %TBPin:474F4F474C457E312E4C4E4B=% /f > NUL 2>&1
+
+reg delete "HKCU\SOFTWARE\RegisteredApplications" /v "Google Chrome" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Clients\StartMenuInternet\Chrome" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Classes\Chrome" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Classes\Chrome.HTML" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Classes\Chrome.XHTML" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Classes\Chrome.SHTML" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Classes\Chrome.PDF" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Classes\Chrome.SVG" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Classes\Chrome.WEBP" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\FTP\UserChoice" /v "ProgID" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice" /v "ProgID" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice" /v "ProgID" /f > NUL 2>&1
+
+reg delete "HKCU\SOFTWARE\Chrome" /f > NUL 2>&1
+reg delete "HKCU\SOFTWARE\Chromium" /f > NUL 2>&1
+
+taskkill /f /im "explorer.exe" > NUL 2>&1
+start "" "explorer.exe" > NUL 2>&1
+explorer "%~dp0" > NUL 2>&1
 exit
